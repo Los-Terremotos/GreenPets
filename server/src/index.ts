cd70f7d806d6f4420dfc0387199c4c4e9c42ead1
Jenny Schmalz <jschmalzperez@gmail.com>
2023-12-18 12:18:12 -0800

    add apollo-server-express package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,27 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer } from "apollo-server-express";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 import resolvers from "./resolvers";
 import { PlantBasic, PlantExpanded } from "./datasources/plants-api";
+import express, { Request, Response } from "express";
+// import { http } from "http";
+const http = require('http');
+import { graphqlHTTP } from "express-graphql";
+
+const PORT = process.env.PORT || 3000;
+
+// set up basic Express server
+// needed to set app to type any to get rid of following error on line 40:
+  // type 'express' is not assignable to type 'application'. types of property 'get' are incompatible.
+const app: any = express();
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello, Express!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Express server running on http://localhost:${PORT}`);
+// });
 
 
 interface ContextValue {
@@ -17,32 +36,57 @@ const getTokenFromRequest = (req: any): string => {
   return req.headers.authorization || "";
 };
 
-const server = new ApolloServer<ContextValue>({
-  typeDefs,
-  resolvers,
+// Create an Apollo Server and integrate it with Express
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express!');
 });
 
-(async () => {
-  const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => {
-      const token = getTokenFromRequest(req);
-      const { cache } = server;
-      return {
-        token,
-        dataSources: {
-          plantBasic: new PlantBasic({ cache, token }),
-          plantExpanded: new PlantExpanded({ cache, token }),
-        },
-      };
-    },
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`GraphQL endpoint at http://localhost:${PORT}/graphql`);
+});
 
-  console.log(` 
-    🌺 Server is running!
-    Grow! Grow!! GROWW!!! 🦠🐸🐲
-    Server ready at ${url}
-  `);
-})();
+// const apolloServer = new ApolloServer<ContextValue>({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const httpServer = http.createServer(app);
+
+// apolloServer.applyMiddleware({ app, path:'/graphql' });
+
+// apolloServer.installSubscriptionHandlers(httpServer);
+
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Express server running on http://localhost:${PORT}`);
+// });
+
+
+// (async () => {
+//   const { url } = await startStandaloneServer(server, {
+//     context: async ({ req }) => {
+//       const token = getTokenFromRequest(req);
+//       const { cache } = server;
+//       return {
+//         token,
+//         dataSources: {
+//           plantBasic: new PlantBasic({ cache, token }),
+//           plantExpanded: new PlantExpanded({ cache, token }),
+//         },
+//       };
+//     },
+//   });
+
+//   console.log(` 
+//     🌺 Server is running!
+//     Grow! Grow!! GROWW!!! 🦠🐸🐲
+//     Server ready at ${url}
+//   `);
+// })();
 
 
 // UNCOMMENT BELOW CODE TO USE MOCK DATA WITH APOLLO SERVER
