3502ef7c27f1b8f24b552ed12b29bb53749cc963
Cristian Corrales <crisdevs95@gmail.com>
2024-01-14 23:58:31 -0500

    continue merge

--- client/src/App/store.ts
+++ client/src/App/store.ts
@@ -1,35 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import plantTypeReducer from '../Features/plantType/plantTypeSlice';
-<<<<<<< HEAD
 import questionsSliceReducer from '../Features/Questions/questionsSlice';
 import responseSliceReducer from '../Features/Response/responseSlice';
 import setQueryResReducer from '../Features/QueryResult/queryResultSlice';
-
-export const store = configureStore({
-=======
 import userAuthReducer from '../Features/userAuth/authSlice';
 import blahBlah from '../Features/playTime/playTestSlice';
 import modalReducer from '../Features/modal/modalSlice';
 import loginReducer from '../Features/userAuth/loginSlice';
 import signUpReducer from '../Features/userAuth/signUpSlice';
 
-const store = configureStore({
->>>>>>> dev
+export const store = configureStore({
   reducer: {
     //add reducer information
     //plantType: plantTypeReducer incase we want to update the state of the plant type from indoor to outdoor
     plantType: plantTypeReducer,
-<<<<<<< HEAD
     questions: questionsSliceReducer,
     response: responseSliceReducer,
-    queryResult: setQueryResReducer
-=======
+    queryResult: setQueryResReducer,
     userAuth: userAuthReducer,
     testField: blahBlah,
     modalToggle: modalReducer,
     loginToggle: loginReducer,
     signUpToggle: signUpReducer,
->>>>>>> dev
   },
 })
 
