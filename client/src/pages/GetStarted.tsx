eedcbe113a8dd0c73ddd34eba8d69baf1b50a584
Cristian Corrales <crisdevs95@gmail.com>
2024-01-14 19:06:42 -0500

    Removed comments

--- client/src/pages/GetStarted.tsx
+++ client/src/pages/GetStarted.tsx
@@ -34,40 +34,6 @@ h1{
   color: #404337;
 }
 `
-// interface Questions {
-//   name: string,
-//   question: string,
-//   options: Array<string>
-// }
-
-// const questionZero: QuestionsType = {
-//   name: "start",
-//   question: "Let's find you a new green Pet!",
-//   options: ["Begin"]
-// }
-// const questionOne: QuestionsType  = {
-//   name: 'indoor',
-//   question: 'Indoor or Outdoor Plant?',
-//   options: ['outdoor','indoor']
-// }
-// const questionTwo: QuestionsType = {
-//   name: 'watering',
-//   question: 'How Green is your thumb?',
-//   options: [
-//     'A little green', 
-//     'Averagely Green',
-//     'Very green'
-//   ]
-// }
-// const questionThree: QuestionsType = {
-//   name: 'loading',
-//   question: 'Loading....',
-//   options: []
-// }
-
-
-// const questionsArr : Array <QuestionsType> = [questionZero, questionOne, questionTwo, questionThree];
-
 
 const GetStarted = () => {
   console.log("render");
