c6b6087993418bccd655624383a93cef2a9e7cdb
Cristian Corrales <crisdevs95@gmail.com>
2024-01-22 17:37:11 -0500

    Added contribution to the homepage component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,10 +4,11 @@ import leaf from '../assets/herbal-spa-treatment-leaves.png'
 import graph from '../assets/graph.jpg';
 import lineGraph from '../assets/line-graph.jpg'
 import search from '../assets/search.jpg'
-import Navbar from '../components/Navbar'
-import Contribution from '../components/Contributions';
+import Navbar from '../components/Navbar';
 import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 // global style specific to this component
@@ -175,6 +176,47 @@ const AboutSection = styled.div`
   background-color: purple;
 
 `
+const ContributionContainer = styled.div`
+height: 100vh;
+display: grid;
+grid-template-rows: 1fr 1fr
+grid-template-columns: 1fr;
+background-color: #b6ad90;
+
+color: #fff;
+
+    h1{
+     text-align:center;
+     padding-top:50px;
+    }
+
+    li{
+        list-style: none;
+        text-align:center;
+    }
+    .contributor-profile{
+        grid-area: contributor
+    }
+    .contribution-icons{
+        display: flex;
+        font-size: 40px;
+        justify-content: space-around;
+    }
+
+    .icon : hover{
+        color:green
+    }
+`;
+const ContributionImg = styled.img`
+    width: 200px;
+    border-radius: 60%;
+`
+
+const ContributorWrap = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
 
 
 const HomePage: React.FC = () => {
@@ -233,7 +275,55 @@ const HomePage: React.FC = () => {
             </EndContainer>
           </FlexEndContainer>
         </MiddleContainer>
-        <Contribution />
+        <ContributionContainer id = "contribution">
+    <h1>Contributors</h1>
+        <ContributorWrap>
+        <div>
+            <ContributionImg src = "../src/assets/contributors/kp.jpeg"/>
+            <ul>
+                <li>Kevin Phan</li>
+                <li>Full Stack Engineer</li>
+                <div className = "contribution-icons">
+                <li> <a href = "https://github.com/KP824"> <FontAwesomeIcon className="icon" icon={faGithub} style={{color: "black"}}/></a></li>
+                <li><a href = "https://www.linkedin.com/in/kp824/"><FontAwesomeIcon className="icon" icon={faLinkedin} style={{color: "black"}}/></a></li>
+                </div>
+            </ul>
+        </div>
+        <div>
+            <ContributionImg src = "../src/assets/contributors/matt.jpeg"/>
+            <ul>
+                <li>Matt Mattox</li>
+                <li>Front-End Engineer</li>
+                <div className = "contribution-icons">
+                <li><a href = "https://github.com/heyitsmattox"><FontAwesomeIcon className="icon" icon={faGithub} style={{color: "black"}}/></a></li>
+                <li><a href = "https://www.linkedin.com/in/mattmattox12/"><FontAwesomeIcon className = "icon" icon={faLinkedin} style={{color: "black"}}/></a></li>
+                </div>
+            </ul>
+        </div>
+        <div>
+            <ContributionImg src = "../src/assets/contributors/cristian.jpeg"/>
+            <ul>
+                <li>Cristian Corrales</li>
+                <li>Front-End Engineer</li>
+                <div className = "contribution-icons">
+                <li><a href = "https://github.com/crisdevs"><FontAwesomeIcon className = "icon" icon={faGithub} style={{color: "black"}}/></a></li>
+                <li><a href = "https://www.linkedin.com/in/criscorr/"><FontAwesomeIcon className = "icon" icon={faLinkedin} style={{color: "black"}}/></a></li>
+                </div>
+            </ul>
+        </div>
+        <div>
+            <ContributionImg src = "../src/assets/contributors/stephanie.jpeg"/>
+            <ul>
+                <li>Stephanie Serrano</li>
+                <li>Back End Engineer</li>
+                <div className = "contribution-icons">
+                <li><a href = "https://github.com/stephanie-115"><FontAwesomeIcon className = "icon" icon={faGithub} style={{color: "black"}}/></a></li>
+                <li><a href = "https://www.linkedin.com/in/stephanie-t-serrano/"><FontAwesomeIcon className = "icon" icon={faLinkedin} style={{color: "black"}}/></a></li>
+                </div>
+            </ul>
+        </div>
+        </ContributorWrap>
+    </ContributionContainer>
     </>
   )
 }
