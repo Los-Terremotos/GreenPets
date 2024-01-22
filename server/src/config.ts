dcca4ec357fbadaf8e5b930ca1cab5592d73221e
stephanie-115 <stephanie.terese@gmail.com>
2024-01-22 15:45:33 -0500

    Added Redis to server

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,4 +3,7 @@ dotenv.config();
 
 export const PLANT_API = process.env.PLANT_API;
 export const DB_URI = process.env.DB_URI;
+export const REDIS_USERNAME = process.env.REDIS_USERNAME;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_ENDPOINT = process.env.REDIS_ENDPOINT;
 
