f82336b622bfbada168146752b553e770b619796
stephanie-115 <stephanie.terese@gmail.com>
2024-01-29 16:10:27 -0500

    Fixed styles.tsx

--- client/src/styles.tsx
+++ client/src/styles.tsx
@@ -7,7 +7,7 @@ export const GeneralSectionContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: flex;
+  justify-content: center;
   align-items: center;
 `;
 
