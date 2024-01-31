de66b7317b9ab9599ba32a9753aae8e89c709d7a
Cristian Corrales <crisdevs95@gmail.com>
2024-01-31 09:02:11 -0500

    Added call to action section with blurred overlay and added buttons as well as background image

--- client/src/components/HomePageComponents/CallToActionSection.tsx
+++ client/src/components/HomePageComponents/CallToActionSection.tsx
@@ -3,7 +3,40 @@ import styled from 'styled-components';
 import { GeneralSectionContainer } from '../../styles';
 
 const CallToActionContainer = styled(GeneralSectionContainer)`
-  background-color: #CB9B7C;
+background-image: url("../../src/assets/call-to-action.jpg");
+background-position: center;
+background-size: cover;
+width: 100vw;
+height:100vh;
+
+p{
+  color: white;
+  font-size: 1.2rem;
+}
+`;
+
+const CallToActionTitle = styled.h1`
+font-size: 3rem;
+color: #FFFFFF;
+text-shadow: 3px 5px 2px #474747;
+
+`
+const TextContainer = styled.div`
+backdrop-filter: blur(10px);
+height: 500px;
+border-radius: 10px;
+-webkit-box-shadow: 0px 0px 19px 1px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 0px 19px 1px rgba(0,0,0,0.75);
+box-shadow: 0px 0px 19px 1px rgba(0,0,0,0.75);
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 50px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
 `;
 
 const CallToActionSection: React.FC = () => {
@@ -11,9 +44,16 @@ const CallToActionSection: React.FC = () => {
 
   return (
     <CallToActionContainer>
-      <h1>Call to action section</h1>
-      <h3>Add get started button</h3>
-      <h3>Add login / sign up buttons</h3>
+      <TextContainer>
+      <CallToActionTitle>Discover the perfect plant companion for your space!</CallToActionTitle>
+      <p>You can get started below or feel free to sign up or log in now to explore 
+        our curated selection and bring the beauty of nature into your home.</p>
+      <button>Find your Green Pet!</button>
+      <ButtonContainer>
+        <button>Login</button>
+        <button>Sign Up!</button>
+      </ButtonContainer>
+      </TextContainer>
     </CallToActionContainer>
   )
 }; 
