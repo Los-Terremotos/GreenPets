1f31f0a58c3c19d830ca2e56b043f23d8e4100ea
Cristian Corrales <crisdevs95@gmail.com>
2024-02-02 13:56:42 -0500

    Changed background from animation to static background

--- client/src/components/HomePageComponents/FooterSection.tsx
+++ client/src/components/HomePageComponents/FooterSection.tsx
@@ -1,16 +1,34 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 
 
 const FooterContainer = styled.div`
   width: 100%;
-  height: 25vh;
   background-color: #75472F;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(-45deg, #f0ead2, #adc178, #adc178, #6c584c);
+  height: 25vh;
+  color: white;
+
+  h1{
+    color: #FFFFFF;
+    text-shadow: #474747 3px 2px 2px;
+  }
 `;
+
+const GithubIcon = styled.a`
+  font-size: 50px;
+
+  :hover{
+    color: #ffdab9;
+  }
+
+`
 
 
 const FooterSection: React.FC = () => {
@@ -19,8 +37,15 @@ const FooterSection: React.FC = () => {
   return (
     <FooterContainer>
       <h1>Looking for ways to contribute?</h1>
-      <h3>Check out our Repo on github!</h3>
-      <a href = "https://github.com/Los-Terremotos/GreenPets">Github</a>
+      <h3>Check out our Repo below!</h3>
+      <GithubIcon href = "https://github.com/Los-Terremotos/GreenPets">
+                    {" "}
+                    <FontAwesomeIcon
+                      className="icon"
+                      icon={faGithub}
+                      style={{ color: "black" }}
+                    />
+                  </GithubIcon>
     </FooterContainer>
   )
 };
