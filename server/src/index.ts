74e22bce6da446b744d089c25b5e381ad23022c4
KP824 <112226540+KP824@users.noreply.github.com>
2024-02-07 12:38:21 -0800

    Explicity set schema file path in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 //import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "../src/schema";
 import resolvers from "./resolvers";
 import { connect } from "./services/redis";
 import { expressMiddleware } from "@apollo/server/express4";
