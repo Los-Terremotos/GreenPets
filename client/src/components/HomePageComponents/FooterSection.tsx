33654108523cb22da5b4f3a5287afef4b659bcc7
stephanie-115 <stephanie.terese@gmail.com>
2024-02-28 09:22:27 -0500

    Merge with dev: Resolve conflicts

--- client/src/components/HomePageComponents/FooterSection.tsx
+++ client/src/components/HomePageComponents/FooterSection.tsx
@@ -1,54 +1,54 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
-
+import GitHubDiscordSection from "./GitHubDiscordSection";
+import ContactForm from "./ContactForm";
 
 const FooterContainer = styled.div`
   width: 100%;
-  background-color: #75472F;
+  background-color: #75472f;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   background: linear-gradient(-45deg, #f0ead2, #adc178, #adc178, #6c584c);
-  height: 25vh;
+  height: 80vh;
   color: white;
+  position: relative;
+`;
+
+const DividerLine = styled.div`
+  width: 2px;
+  height: 70px;
+  background: red;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const FooterHeading = styled.h1`
-  color: #FFFFFF;
+  color: #ffffff;
   text-shadow: #474747 3px 2px 2px;
 `;
 
-const GithubIcon = styled.a`
-  font-size: 50px;
-
-  :hover{
-    color: #ffdab9;
-  }
-
-`
-
+const ContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+`;
 
 const FooterSection: React.FC = () => {
-
-
   return (
     <FooterContainer>
       <FooterHeading>Looking for ways to contribute?</FooterHeading>
-      <h3>Check out our Repo below!</h3>
-      <GithubIcon href="https://github.com/Los-Terremotos/GreenPets">
-        {" "}
-        <FontAwesomeIcon
-          className="icon"
-          icon={faGithub}
-          style={{ color: "black" }}
-        />
-      </GithubIcon>
+      <DividerLine />
+      <ContentContainer>
+        <GitHubDiscordSection />
+        <ContactForm />
+      </ContentContainer>
     </FooterContainer>
-  )
+  );
 };
-
 
 export default FooterSection;
